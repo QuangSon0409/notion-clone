262f8ea8b7c3a302cc00e41188d84136f155feb8
QuangSon0409 <buiquangson7782@gmail.com>
2024-01-03 16:03:20 +0700

    error when i install @clerk/clerk-react

--- app/(marketing)/_component/Navbar.tsx
+++ app/(marketing)/_component/Navbar.tsx
@@ -3,13 +3,16 @@ import { useScrollTop } from "@/hook/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
 
 const Navbar = () => {
     const scrolled = useScrollTop();
+    // const { isAuthenticated, isLoading } = useConvexAuth()
     return (<>
         <div className={cn("z-50 fixed bg-background flex items-center top-0 w-full p-6", scrolled && "border-b shadow-sm")}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between items-center gap-x-2 w-full flex">
+                {/* {isLoading && (<>Loading</>)} */}
                 <ModeToggle />
             </div>
         </div>
